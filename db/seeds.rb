52ebe34a98e224eac0ba7b1dcc7ae23f3415c8ec
Jake Porter <Jake.m.porter@gmail.com>
2020-09-01 20:14:53 -0500

    used .create shortcut in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
 
-lamp = Product.new(name: "lamp", price: 12, description: "silver lamp with white lamp shade")
-lamp.save
-
-candle = Product.new(name: "candle", price: 16, description: "cinnamon scented candle")
-candle.save
-
-iphone = Product.new(name: "iPhone 11", price: "950", description: "Space Grey Apple iPhone")
-iphone.save
+Product.create(name: "lamp", price: 12, description: "silver lamp with white lamp shade")
+Product.create(name: "candle", price: 16, description: "cinnamon scented candle")
+Product.create(name: "iPhone 11", price: "950", description: "Space Grey Apple iPhone")

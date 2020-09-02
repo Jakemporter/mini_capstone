de7909e2d2350c1b1164f82b0fd2e6d2fd1a1295
Jake Porter <Jake.m.porter@gmail.com>
2020-09-01 19:18:19 -0500

    added ability to look at all prodcuts on individual pages

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,18 @@
 class Api::ProductsController < ApplicationController
   def allproducts
     @all_products = Product.all
-    render "product.json.jb"
+    render "all.json.jb"
+  end
+  def candle
+    @candle = Product.find_by(name: "candle")
+    render "candle.json.jb"
+  end
+  def lamp
+    @lamp = Product.find_by(name: "lamp")
+    render "lamp.json.jb"
+  end
+  def iphone
+    @iphone = Product.find_by(name: "iPhone 11")
+    render "iphone.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/allproducts" => "products#allproducts"
+    get "/candle" => "products#candle"
+    get "/lamp" => "products#lamp"
+    get"/iphone" => "products#iphone"
   end
 end

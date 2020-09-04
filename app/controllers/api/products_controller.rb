80bc0f71a539f7cc09d04677db560b3c43b943f0
Jake Porter <Jake.m.porter@gmail.com>
2020-09-03 21:00:10 -0500

    added create and update features

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,9 +3,27 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "index.json.jb"
   end
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    @product.save
+    render "show.json.jb"
+  end
   def show
-    input = params["id"]
-    @product = Product.find_by(id: input)
+    @product = Product.find_by(id: params["id"])
+    render "show.json.jb"
+  end
+  def update
+    @product = Product.find_by(id: params["id"]) 
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    @product.save
     render "show.json.jb"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 
 Product.create(name: "lamp", price: 12, description: "silver lamp with white lamp shade")
 Product.create(name: "candle", price: 16, description: "cinnamon scented candle")
-Product.create(name: "iPhone 11", price: "950", description: "Space Grey Apple iPhone")
+Product.create(name: "iPhone 11", price: 950, description: "Space Grey Apple iPhone")

cc50e0374a4c522249d8448b01566e7137a70e56
Jake Porter <Jake.m.porter@gmail.com>
2020-09-07 20:59:01 -0500

    made price a decimal in the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_015028) do
+ActiveRecord::Schema.define(version: 2020_09_08_015343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 12, scale: 2
     t.string "image_url"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false

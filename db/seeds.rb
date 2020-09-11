991056dfd679dfe1437d1b061533078f819c033f
Jake Porter <Jake.m.porter@gmail.com>
2020-09-10 23:52:34 -0500

    added more to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@ Product.create!(name: "lamp", price: 12, description: "silver lamp with white la
 Product.create!(name: "candle", price: 16, description: "cinnamon scented candle", quantity: 45, supplier_id: 2)
 Product.create!(name: "iPhone 11", price: 950, description: "Space Grey Apple iPhone", quantity: 43, supplier_id: 1)
 Product.create!(name: "iPad", price: 999, description: "Space Grey Apple iPhone", quantity: 54, supplier_id: 1)
+Product.create!(name: "Apple Keyboard", price: 129, description: "Space Grey Apple Keyboard with number pad", quantity: 45, supplier_id: 1)
+Product.create!(name: "Amazon Echo", price: 49.99, description: "Amazon Echo Dot", quantity: 238, supplier_id: 2)
 
 Image.create!(product_id: 3, url: "https://www.att.com/catalog/en/idse/Apple/Apple%20iPhone%2011%
   0Pro/Midnight%20Green-hero-zoom.png")
@@ -13,3 +15,5 @@ Image.create!(product_id: 3, url: "https://encrypted-tbn0.gstatic.com/images?q=t
 Image.create!(product_id: 1, url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSfy3XjkmsCt4AIUPfXqXw526BWBU1w9FMk3w&usqp=CAU")
 Image.create!(product_id: 2, url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFsVSd6DMtKPq67oE81U5KcrGpTb_y03yo9Q&usqp=CAU")
 Image.create!(product_id: 4, url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRPvsx29MB8-tLW4Ckdddr4yDFcMHJW2rMViw&usqp=CAU")
+Image.create!(product_id: 5, url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvH3wZglFVcL7J0Sl3Gal4vTB3TW6ryyF_Jg&usqp=CAU")
+Image.create!(product_id: 6, url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJ5XWPU5cdhY4uF0_p82FpAaDwCtW2NxxVow&usqp=CAU")

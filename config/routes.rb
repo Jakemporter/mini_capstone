e53f89ae80b8425e06291131e0400a7059291988
Jake Porter <Jake.m.porter@gmail.com>
2020-09-10 20:28:24 -0500

    made suppliers CRUD capable and edited seeds file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/products" => "products#index"
-    post "/products" => "products#create"
     get "/products/:id" => "products#show"
+    post "/products" => "products#create"
     patch "/products/:id" => "products#update"
-    delete "products/:id" => "products#destroy"
+    delete "/products/:id" => "products#destroy"
+    get "/suppliers" => "suppliers#index"
+    get "/suppliers/:id" => "suppliers#show"
+    post "/suppliers" => "suppliers#create"
+    patch "/suppliers/:id" => "suppliers#update"
+    delete "/suppliers/:id" => "suppliers#destroy"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
+Supplier.create!(name: "Apple", email: "Apple@example.com", phone_number: "555-123-4321")
+Supplier.create!(name: "Amazon", email: "Amazon@example.com", phone_number: "555-321-1234")
 
-Product.create(name: "lamp", price: 12, description: "silver lamp with white lamp shade")
-Product.create(name: "candle", price: 16, description: "cinnamon scented candle")
-Product.create(name: "iPhone 11", price: 950, description: "Space Grey Apple iPhone")
+
+Product.create!(name: "lamp", price: 12, description: "silver lamp with white lamp shade", quantity: 32, supplier_id: 2)
+Product.create!(name: "candle", price: 16, description: "cinnamon scented candle", quantity: 45, supplier_id: 2)
+Product.create!(name: "iPhone 11", price: 950, description: "Space Grey Apple iPhone", quantity: 43, supplier_id: 1)

7ffe9ef2b72c2e708534cd101c9744d837fcd0f5
Jake Porter <Jake.m.porter@gmail.com>
2020-09-13 14:00:26 -0500

    added validation for images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ApplicationRecord
+  validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg|CAU)$}i, :message => "must have a valid filetype", multiline: true  
+  
 
   belongs_to :product
   

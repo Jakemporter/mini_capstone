ba5ccc6eb584adb8a4a4280a9c2cb94aff572f0a
Jake Porter <Jake.m.porter@gmail.com>
2020-09-15 21:25:51 -0500

    fixed bug that didn't allow pictures to save to database

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
-  validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg|CAU)$}i, :message => "must have a valid filetype", multiline: true  
+  validates_format_of :url, :with => %r{\.(png|jpg|jpeg|CAU)$}i, :message => "must have a valid filetype", multiline: true  
   
 
   belongs_to :product

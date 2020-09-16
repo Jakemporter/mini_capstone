98fd54a948b6101ea07aef236d7c0ab478205d56
Jake Porter <Jake.m.porter@gmail.com>
2020-09-15 19:19:33 -0500

    added order total/subtotal ect and added ability to view all orders

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,15 +1,29 @@
 class Api::OrdersController < ApplicationController
   def create
     if current_user
-      Order.create!(
+      product = Product.find_by(id: params[:product_id])
+      subtotal = product.price * params[:quantity].to_i
+      tax = subtotal * 0.09
+      order = Order.create!(
         user_id: current_user.id,
         product_id: params[:product_id],
         quantity: params[:quantity],
-        subtotal: Product.find_by(id: :product_id).price * :quantity,
+        subtotal: product.price * params[:quantity].to_i,
+        tax: subtotal * 0.09,
+        total: subtotal + tax
         )
-      render json: {message: "product purchased"}
+      render json: {order: order}
     else
       render json: {message: "must be logged in"}
+    end
+  end
+
+  def index
+    if current_user
+      @orders = Order.where(user_id: current_user.id)
+      render "index.json.jb"
+    else
+      render json: {denied: "Must be logged in to view orders"}
     end
   end
 end

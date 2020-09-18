ccf8762a0627783057b3016b294b2c8a25054fd5
Jake Porter <Jake.m.porter@gmail.com>
2020-09-17 20:08:58 -0500

    added create method in controller

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -1,2 +1,11 @@
 class Api::CartedProductsController < ApplicationController
+  before_action :authenticate_user
+  def create
+    CartedProduct.new(
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      user_id: current_user.id,
+      status: "carted"
+    )
+  end
 end

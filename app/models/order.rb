a650c7c1e0e797a0cc1da68209e9369388934cf9
Jake Porter <Jake.m.porter@gmail.com>
2020-09-22 18:31:01 -0500

    tried fixing frontend show and refractored code to make it cleaner

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,16 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :carted_products
+
+  def update_totals
+    calculated_subtotal = 0
+    calculated_tax = 0
+    calculated_total = 0
+    carted_products.map do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+      calculated_tax += carted_product.quantity * carted_product.product.tax
+    end
+    calculated_total += calculated_subtotal + calculated_tax
+    update(subtotal: calculated_subtotal, tax: calculated_tax, total: calculated_total)
+  end
 end
